8411993717ceb3844be9212317e4fd95b66b60eb
Ayush Amberkar <ayushempire03@gmail.com>
2024-09-24 01:06:49 +0530

    chnged proxy

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
+
  
 dotenv.config();
 

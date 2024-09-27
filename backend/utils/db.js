7909919a5dd18f247e5d61ecbf498565aa6a62c7
thecodebuddy <beldarjafar@gmail.com>
2024-09-27 04:05:29 +0000

    bug fixes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://ayush:ayush123@db.mq3x6.mongodb.net/");
         console.log('mongodb connected successfully.');
     } catch (error) {
         console.log(error);
